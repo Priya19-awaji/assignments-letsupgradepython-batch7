7dab91da1270b68e2e6a07a1a6dce13453be0d36
Priya19-awaji <70806204+Priya19-awaji@users.noreply.github.com>
2020-09-12 19:25:01 +0530

    Add files via upload

---
+++ assignment1 day 4.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Program to check Armstrong numbers in a certain interval
+
+lower = 104200
+upper = 702648265
+
+for num in range(lower, upper + 1):
+
+   # order of number
+   order = len(str(num))
+    
+   # initialize sum
+   sum = 0
+
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)
+
